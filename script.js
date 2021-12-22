d92b4528e93bd61bcd3e2b027cd1aea3e73b333a
Glory <koyannaingkyawtint@gmail.com>
2021-12-22 21:14:20 +0630

    Adding JS and styling and animation

---
+++ script.js
@@ -0,0 +1,19 @@
+var counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+   counter.innerHTML = '0'
+
+   var updateCounter = () =>{
+      const target = +counter.getAttribute('data-target')
+      const c = +counter.innerText
+
+      const increment = target / 500
+      if(c < target) {
+         counter.innerText = `${Math.ceil(c + increment)}`
+         setTimeout(updateCounter, 1)
+      } else{
+         counter.innerText = target
+      }
+   }
+   updateCounter()
+})
